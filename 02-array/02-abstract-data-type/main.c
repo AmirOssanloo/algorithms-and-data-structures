34d961c21296a283555b6d2ad14f349ffe4fc32f
Amir Ossanloo <6134758+AmirOssanloo@users.noreply.github.com>
2020-05-19 11:39:55 -0300

    Add Union, Intersection, Difference, and Merge functions to array

--- 02-array/02-abstract-data-type/main.c
+++ 02-array/02-abstract-data-type/main.c
@@ -7,10 +7,23 @@ struct Array {
   int length;
 };
 
+
 void swap(int *x, int *y) {
   int temp = *x;
   *x = *y;
   *y = temp;
+};
+
+int isSorted(struct Array arr) {
+  int i;
+
+  for (i = 0; i < arr.length - 1; i++) {
+    if (arr.a[i] > arr.a[i + 1]) {
+      return 0;
+    }
+  }
+
+  return 1;
 };
 
 void Display(struct Array arr) {
@@ -75,6 +88,19 @@ void Reverse(struct Array *arr) {
   }
 };
 
+void Rearrange(struct Array *arr) {
+  int i = 0;
+  int j = arr->length - 1;
+
+  while (i < j) {
+    while (arr->a[i] < 0) i++;
+    while (arr->a[j] >= 0) j--;
+
+
+    if (i < j) swap(&arr->a[i], &arr->a[j]);
+  }
+};
+
 int LinearSearch(struct Array *arr, int key) {
   int i;
 
@@ -87,7 +113,7 @@ int LinearSearch(struct Array *arr, int key) {
   }
 
   return -1;
-}
+};
 
 int BinarySearch(struct Array arr, int key) {
   int low = 0;
@@ -106,7 +132,158 @@ int BinarySearch(struct Array arr, int key) {
   }
 
   return -1;
-}
+};
+
+void LeftShift(struct Array *arr) {
+  int temp;
+  int i;
+
+  for (i = 0; i < arr->length; i++) {
+    if (i < arr->length - 1) arr->a[i] = arr->a[i + 1];
+    else arr->a[i] = 0;
+  }
+};
+
+void RightShift(struct Array *arr) {
+  int temp;
+  int i;
+
+  for (i = arr->length - 1; i >= 0; i--) {
+    if (i > 0) arr->a[i] = arr->a[i - 1];
+    else arr->a[i] = 0;
+  }
+};
+
+void InsertSort(struct Array *arr, int x) {
+  int i = arr->length - 1;
+
+  if (arr->length == arr->size) {
+    return;
+  }
+
+  while (i >= 0 && arr->a[i] > x) {
+    arr->a[i + 1] = arr->a[i];
+    i--;
+  }
+
+  arr->a[i + 1] = x;
+  arr->length++;
+};
+
+struct Array* Merge(struct Array *arr1, struct Array *arr2) {
+  int i = 0;
+  int j = 0;
+  int k = 0;
+
+  struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+
+  while (i < arr1->length && j < arr2->length) {
+    if (arr1->a[i] < arr2->a[j]) {
+      arr3->a[k++] = arr1->a[i++];
+    } else {
+      arr3->a[k++] = arr2->a[j++];
+    }
+  }
+
+  for(; i < arr1->length; i++) {
+    arr3->a[k++] = arr1->a[i];
+  }
+
+  for(; j < arr2->length; j++) {
+    arr3->a[k++] = arr2->a[j];
+  }
+
+  arr3->length = arr1->length + arr2->length;
+  arr3->size = arr3->length;
+
+  return arr3;
+};
+
+struct Array* Union(struct Array *arr1, struct Array *arr2) {
+  int i = 0;
+  int j = 0;
+  int k = 0;
+
+  struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+
+  while (i < arr1->length && j < arr2->length) {
+    if (arr1->a[i] < arr2->a[j]) {
+      arr3->a[k++] = arr1->a[i++];
+    } else if (arr2->a[j] < arr1->a[i]) {
+      arr3->a[k++] = arr2->a[j++];
+    } else {
+      arr3->a[k++] = arr1->a[i++];
+      j++;
+    }
+  }
+
+  for(; i < arr1->length; i++) {
+    arr3->a[k++] = arr1->a[i];
+  }
+
+  for(; j < arr2->length; j++) {
+    arr3->a[k++] = arr2->a[j];
+  }
+
+  arr3->length = k;
+  arr3->size = arr3->length;
+
+  return arr3;
+};
+
+struct Array* Intersection(struct Array *arr1, struct Array *arr2) {
+  int i = 0;
+  int j = 0;
+  int k = 0;
+
+  struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+
+  while (i < arr1->length && j < arr2->length) {
+    if (arr1->a[i] < arr2->a[j]) {
+      i++;
+    } else if (arr2->a[j] < arr1->a[i]) {
+      j++;
+    } else {
+      arr3->a[k++] = arr1->a[i++];
+      j++;
+
+    }
+  }
+
+  arr3->length = k;
+  arr3->size = arr3->length;
+
+  return arr3;
+};
+
+struct Array* Difference(struct Array *arr1, struct Array *arr2) {
+  int i = 0;
+  int j = 0;
+  int k = 0;
+
+  struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+
+  while (i < arr1->length && j < arr2->length) {
+    if (arr1->a[i] < arr2->a[j]) {
+      arr3->a[k++] = arr1->a[i++];
+    } else if (arr2->a[j] < arr1->a[i]) {
+      j++;
+    } else {
+      i++;
+      j++;
+    }
+  }
+
+  for(; i < arr1->length; i++) {
+    arr3->a[k++] = arr1->a[i];
+  }
+
+  arr3->length = k;
+  arr3->size = arr3->length;
+
+  return arr3;
+};
+
 
 int Get(struct Array arr, int index) {
   if (index >= 0 && index < arr.length) {
@@ -167,26 +344,11 @@ float Avg(struct Array arr) {
 };
 
 int main() {
-  struct Array arr = {{2, 3, 4, 5, 6, 8, 10, 12, 14, 16, 18}, 20, 11};
+  struct Array arr1 = {{2, 3, 4, 6, 7, 8, 12, 14}, 20, 8};
+  struct Array arr2 = {{1, 3, 7, 10, 12, 14}, 20, 6};
+  struct Array *arr3;
 
-  Display(arr);
-  Append(&arr, 8);
-  Display(arr);
-  Insert(&arr, 1, 0);
-  Display(arr);
-  printf("Delete element: %d\n", Delete(&arr, 1));
-  Display(arr);
-  printf("LinearSearch element: %d\n", LinearSearch(&arr, 5));
-  printf("BinarySearch element: %d\n", BinarySearch(arr, 14));
-  printf("Set element: %d\n", Set(&arr, 4, 11));
-  printf("Get element: %d\n", Get(arr, 4));
-  printf("Min element: %d\n", Min(arr));
-  printf("Max element: %d\n", Max(arr));
-  printf("Sum of elements: %d\n", Sum(arr));
-  printf("Avg of elements: %f\n", Avg(arr));
-  Display(arr);
-  Reverse(&arr);
-  Display(arr);
+  Display(*arr3);
 
   return 0;
 };
